f22c7943845ce4c8199712206a92314fba522c8d
Menardo Madiumba <menardotera@gmail.com>
2025-11-24 17:55:45 +0100

    Add validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,37 @@ class AuthController extends Controller
 
     public function loginHandler(Request $request)
     {
-        dd($request->all());
+        $fielType = filter_var($request->input('login_id'), FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        if ($fielType == 'email') {
+
+            $request->validate([
+                'login_id' => ['required', 'email', 'exists:users,email'],
+                'password' => ['required', 'min:5'],
+            ], [
+                'login_id.required' => 'Veuillez saisir votre adresse email.',
+                'login_id.email' => 'Le format de l\'adresse email est invalide.',
+                'login_id.exists' => 'Aucun compte n’est associé à cette adresse email.',
+
+                'password.required' => 'Veuillez entrer votre mot de passe.',
+                'password.min' => 'Le mot de passe doit contenir au moins 5 caractères.',
+            ]);
+
+        } else {
+
+            $request->validate([
+                'login_id' => ['required', 'exists:users,username'],
+                'password' => ['required', 'min:5'],
+            ], [
+                'login_id.required' => 'Veuillez saisir votre nom d’utilisateur.',
+                'login_id.exists' => 'Aucun compte n’est associé à ce nom d’utilisateur.',
+                'password.required' => 'Veuillez entrer votre mot de passe.',
+                'password.min' => 'Le mot de passe doit contenir au moins 5 caractères.',
+            ]);
+
+
+        }
+
     }
 
     public function forgot_form(Request $request)
